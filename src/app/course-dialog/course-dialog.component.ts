a0d1b0b18c80e30b7d8723d48174aa1b315de77d
NAGARRO\ayushiverma <ayushi.verma@nagarro.com>
2022-07-31 20:59:09 +0530

    loading impl in course-dialog

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -6,11 +6,15 @@ import * as moment from 'moment';
 import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
 import { CoursesService } from '../services/courses.service';
+import { LoadingService } from '../loading/loading.service';
 
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+    styleUrls: ['./course-dialog.component.css'],
+    providers: [
+        LoadingService
+    ]
 })
 export class CourseDialogComponent implements AfterViewInit {
 
@@ -22,7 +26,8 @@ export class CourseDialogComponent implements AfterViewInit {
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) course:Course,
-        private coursesService: CoursesService) {
+        private coursesService: CoursesService,
+        private loading: LoadingService) {
 
         this.course = course;
 
@@ -32,7 +37,6 @@ export class CourseDialogComponent implements AfterViewInit {
             releasedAt: [moment(), Validators.required],
             longDescription: [course.longDescription,Validators.required]
         });
-
     }
 
     ngAfterViewInit() {
@@ -41,9 +45,15 @@ export class CourseDialogComponent implements AfterViewInit {
 
     save() {
       const changes = this.form.value;
-      this.coursesService.saveCourse(this.course.id, changes).subscribe(val => {
-          this.dialogRef.close(val);
-      });
+      const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes);
+
+      this.loading.showLoaderUntilCompleted(saveCourse$).subscribe(val => {
+        this.dialogRef.close(val);
+    });
+
+    //   this.coursesService.saveCourse(this.course.id, changes).subscribe(val => {
+    //       this.dialogRef.close(val);
+    //   });
     }
 
     close() {
